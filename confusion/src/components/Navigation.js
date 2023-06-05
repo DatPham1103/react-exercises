7fbc96e7faca38016b97de1bdc592d800e51ed1e
DatPham1103 <phamtdat1103@gmail.com>
2023-06-06 00:01:05 +0700

    useEffect

--- confusion/src/components/Navigation.js
+++ confusion/src/components/Navigation.js
@@ -1,25 +1,46 @@
 import React from "react";
+import { useContext } from 'react';
+import { ThemeContext } from './ThemeContext';
 export default function Navigation() {
+    const { theme, toggle, dark } = useContext(ThemeContext)
     return (
         <div className="header-container">
-            <nav className="header">
+            <nav className="header" style={{ backgroundColor: theme.backgroundColor, color: theme.color }}>
                 <ul>
                     <li>
-                        <a className="active" href="#home">
+                        <a className="active" href="#home" style={{ color: theme.color }}>
                             Home
                         </a>
                     </li>
                     <li>
-                        <a href="#news">News</a>
+                        <a style={{ color: theme.color }} href="#news">News</a>
                     </li>
                     <li>
-                        <a href="#about">About</a>
+                        <a style={{ color: theme.color }} href="#about">About</a>
                     </li>
                     <li>
-                        <a href="#contact">Contact</a>
+                        <a style={{ color: theme.color }} href="#contact">Contact</a>
                     </li>
+                
+
+                <div style={{ position: "relative"}}>
+                    <a
+                        className="switch-mode"
+                        href="#"
+                        onClick={toggle}
+                        style={{
+                            backgroundColor: theme.backgroundColor,
+                            color: theme.color,
+                            outline: "none",
+                            marginLeft:"83em"
+                        }}
+                        data-testid="toggle-theme-btn"
+                    >
+                        Switch Nav to {!dark ? "Dark" : "Light"} mode
+                    </a>
+                </div>
                 </ul>
             </nav>
         </div>
-    );
+    )
 }
