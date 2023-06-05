3420c4b89a80b0a164e74e4d3f311546db716e09
DatPham1103 <phamtdat1103@gmail.com>
2023-06-05 22:00:25 +0700

    Functional Component

--- confusion/src/components/Players.js
+++ confusion/src/components/Players.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import { Players } from '../shared/ListOfPlayers'
+import React from "react";
+import { Players } from "../shared/ListOfPlayers";
 export default function Player() {
   return (
-    <div className='container'>
+    <div className="container">
       {Players.map((player) => (
-        <div className='column'>
-          <div className='card'>
+        <div className="column">
+          <div className="card">
             <img src={player.img} />
             <h3>{player.name}</h3>
-            <p className='title'>{player.club}</p>
-            <p><button>Details</button></p>
+            <p className="title">{player.club}</p>
+            <p>
+              <button>Detail</button>
+            </p>
           </div>
         </div>
       ))}
