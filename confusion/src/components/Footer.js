a552db42ea3bfbad3612018e169f321738e42b94
DatPham1103 <phamtdat1103@gmail.com>
2023-06-05 16:31:06 +0700

    commit exercise 6

---
+++ confusion/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+export default class footer extends Component {
+    render() {
+        return (
+            <footer>
+            <p>Copyright © 2023</p>
+        </footer>
+        )
+    }
+}
