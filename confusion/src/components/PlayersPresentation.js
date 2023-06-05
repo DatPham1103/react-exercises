1e3d2daab4e6ce07f814a56e23a49599118a1421
DatPham1103 <phamtdat1103@gmail.com>
2023-06-05 22:41:11 +0700

    useState

--- confusion/src/components/PlayersPresentation.js
+++ confusion/src/components/PlayersPresentation.js
@@ -1,17 +1,42 @@
-import React from 'react'
-export default function PlayersPresentation({players}) {
-     //console.log(players);
-    return (
-     <div className='container'>
-        {players.map((player)=>(
-        <div className='column'>
-        <div className='card'>
-        <img src={player.img}/>
-          <h3>{player.name}</h3>
-          <p className='title'>{player.club}</p>
-          <p><button>Detail</button></p>
+import React from "react";
+import { useState } from "react";
+export default function PlayersPresentation({ players }) {
+  const [player, setPlayer] = useState([]);
+  //console.log(players);
+  return (
+    <div className="container">
+      {players.map((player) => (
+        <div className="column">
+          <div className="card">
+            <img src={player.img} />
+            <h3>{player.name}</h3>
+            <p className="title">{player.club}</p>
+            <p>
+              <div className="detail">
+                <a href="#popup1" id="openPopUp">
+                  <button
+                    onClick={() => {
+                      setPlayer(player);
+                    }}
+                  >
+                    <a href='#popup1' id='openPopUp' style={{color:"white",textDecoration:"none"}}>Detail</a>
+                  </button>
+                </a>
+              </div>
+            </p>
+          </div>
+        </div>
+      ))}
+      <div id="popup1" className="overlay">
+        <div className="popup">
+          <img src={player.img} alt="#" />
+          <h2>{player.name}</h2>
+          <a className="close" href="#">
+            &times;
+          </a>
+          <div className="content">{player.info}</div>
         </div>
       </div>
-     ))}</div>   
-  )
+    </div>
+  );
 }
